35f4c42f274c331036a7b9a9ca2cbe60f9a907b3
kangw3n <weitochearyeah@hotmail.com>
2021-03-12 16:23:01 +0800

    add log checker

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ var CronJob = require('cron').CronJob;
 var job = new CronJob(
   '00 50 23 * * *',
   function () {
-    // console.log("test");
+    console.log('test');
     fetch('https://ivftpe.com/api/scheduler.php');
   },
   null,
@@ -21,5 +21,7 @@ app.get('/', async (req, res) => {
     message: 'GCP API-Services Available 2',
   });
 });
+
+console.log('start my node version');
 
 app.listen(process.env.PORT || 8080);
