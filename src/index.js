a8afd333c34ea9c6ac80616eccad523b1bc5236d
kangw3n <weitochearyeah@hotmail.com>
2021-03-12 16:36:37 +0800

    added slack integration

--- src/index.js
+++ src/index.js
@@ -2,13 +2,26 @@ const express = require('express');
 
 const app = express();
 
+const { IncomingWebhook } = require('@slack/webhook');
+
+// Read a url from the environment variables
+const url =
+  'https://hooks.slack.com/services/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';
+
+// Initialize
+const webhook = new IncomingWebhook(url);
+
 var CronJob = require('cron').CronJob;
 var job = new CronJob(
   '00 50 23 * * *',
-  function () {
+  async function () {
     const d = new Date();
     console.log('Midnight Schedular run:', d);
     fetch('https://ivftpe.com/api/scheduler.php');
+    await webhook.send({
+      text: 'Midnight ivftpe Schedular run:',
+      d,
+    });
   },
   null,
   true,
@@ -23,6 +36,10 @@ app.get('/', async (req, res) => {
   });
 });
 
-console.log('start my node version');
-
-app.listen(process.env.PORT || 8080);
+(async () => {
+  await webhook.send({
+    text: 'ivftpe server started.',
+  });
+  console.log('start my node version');
+  app.listen(process.env.PORT || 8080);
+})();
